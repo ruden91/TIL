0262e1be24ca6e90f21f84499a87fe1cee35c67f
ruden91 <ruden91@naver.com>
2019-03-30 17:15:14 +0900

    implement  graph data structure(es6 version) add vertex, add edge

---
+++ 자료구조/Graph/index.js
@@ -0,0 +1,49 @@
+class Graph {
+  constructor(size) {
+    // size, adjacent list 초기섲렁
+    this.size = size;
+    this.adjList = new Map();
+  }
+
+  // 그래프에 정점을 추가하는 메서드
+  addVertex(v) {
+    this.adjList.set(v, []);
+  }
+
+  // 그래프에 엣지를 추가하는 메서드
+  addEdge(v, w) {
+    this.adjList.get(v).push(w);
+    this.adjList.get(w).push(v);
+  }
+
+  printGraph() {
+    const keys = this.adjList.keys();
+
+    for (let key of keys) {
+      const values = this.adjList.get(key);
+      const connectedValues = values.join('');
+      
+      console.log(`${key} -> ${connectedValues}`);
+    }
+  }
+}
+
+const vertices = [ 'A', 'B', 'C', 'D', 'E', 'F' ]; 
+const g = new Graph(vertices.length);
+
+// 정점 추가
+for (let vertex of vertices) {
+  g.addVertex(vertex);
+}
+
+// 엣지 추가
+g.addEdge('A', 'B'); 
+g.addEdge('A', 'D'); 
+g.addEdge('A', 'E'); 
+g.addEdge('B', 'C'); 
+g.addEdge('D', 'E'); 
+g.addEdge('E', 'F'); 
+g.addEdge('E', 'C'); 
+g.addEdge('C', 'F'); 
+
+g.printGraph();
