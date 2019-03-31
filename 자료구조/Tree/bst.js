d64dbb6ea80cdee35cd7586466556a8206cd1029
ruden91 <ruden91@naver.com>
2019-04-01 00:02:34 +0900

    implement BST in javascript (insert method)

---
+++ 자료구조/Tree/bst.js
@@ -0,0 +1,80 @@
+/**
+ * 이진 탐색 트리
+ */
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(data) {
+    const node = this.getNode(data);
+    if (!this.root) {
+      this.root = node;
+    } else {
+      this.insertNode(this.root, node)
+    }
+  }
+
+  remove(data) {
+
+  }
+
+  // 중위순회
+  inorder() {
+
+  }
+
+  // 후위순회
+  postorder() {
+
+  }
+
+  // 전위순회
+  preorder() {
+
+  }
+  
+  printBST() {
+    console.log(JSON.stringify(this.root));
+  }
+  // helpers
+  insertNode(node, newNode) {
+    if (node.data < newNode.data) {
+      // right
+      if (node.right === null) {
+        node.right = newNode;       
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    } else {
+      // left
+      if (node.left === null) {
+        node.left = newNode;        
+      } else {
+        this.insertNode(node.left, newNode)        
+      }
+    }
+  }
+
+  getNode(data) {
+    return {
+      data,
+      left: null,
+      right: null
+    }
+  }
+}
+
+const bst = new BST();
+
+
+bst.insert(5);
+bst.insert(7);
+bst.insert(2);
+bst.insert(14);
+bst.insert(30);
+bst.insert(21);
+bst.insert(3);
+bst.insert(2);
+
+bst.printBST();
