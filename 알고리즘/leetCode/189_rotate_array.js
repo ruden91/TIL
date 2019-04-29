21c563466383475b4b6705bd2d0beb0ed54e5c42
ruden91 <ruden91@naver.com>
2019-04-29 18:12:10 +0900

    solve rotate_array in javascript

---
+++ 알고리즘/leetCode/189_rotate_array.js
@@ -0,0 +1,37 @@
+/**
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = function(nums, k) {
+  for (let i = 0; i < k; i++) {
+    nums.unshift(nums.pop());
+  }
+
+  return nums;
+};
+
+/**
+  Runtime: 92 ms, faster than 59.57% of JavaScript online submissions for Rotate Array.
+  Memory Usage: 35.2 MB, less than 72.96% of JavaScript online submissions for Rotate Array.
+ */
